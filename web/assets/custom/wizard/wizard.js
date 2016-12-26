ca5f779e06baca57103a02ad1aca0395660bfbc9
fafa <fafa@Bouddha>
2016-12-26 16:24:31 +0100

    working on adding a patient

--- web/assets/custom/wizard/wizard.js
+++ web/assets/custom/wizard/wizard.js
@@ -24,14 +24,17 @@ $(document).ready(function () {
         var curStep = $(this).closest(".setup-content"),
             curStepBtn = curStep.attr("id"),
             nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
-            curInputs = curStep.find("input[type='text'],input[type='url']"),
+            curInputs = curStep.find("input[type='text'],input[type='url'],input[type='radio']"),
             isValid = true;
-
+        
+        errorClass="alert alert-danger"
         $(".form-group").removeClass("has-error");
         for(var i=0; i<curInputs.length; i++){
             if (!curInputs[i].validity.valid){
                 isValid = false;
-                $(curInputs[i]).closest(".form-group").addClass("has-error");
+                $(curInputs[i]).closest(".form-group").addClass(errorClass);
+            } else {
+                $(curInputs[i]).closest(".form-group").removeClass(errorClass);
             }
         }
 
