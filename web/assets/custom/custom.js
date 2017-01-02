11769ddd7fe2339ea556882388f3f3c29da821e7
fafa <fafa@Bouddha>
2017-01-02 23:32:32 +0100

    work on add patient view

--- web/assets/custom/custom.js
+++ web/assets/custom/custom.js
@@ -1,56 +1,102 @@
-// Wizard JS
+// Begin Wizard
 
 $(document).ready(function () {
+    //Initialize tooltips
+    $('.nav-tabs > li a[title]').tooltip();
+    
+    //Wizard
+    $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
 
-    var navListItems = $('div.setup-panel div a'),
-            allWells = $('.setup-content'),
-            allPrevBtn = $('.prevBtn'),
-            allNextBtn = $('.nextBtn');
-
-    allWells.hide();
-
-    navListItems.click(function (e) {
-        e.preventDefault();
-        var $target = $($(this).attr('href')),
-                $item = $(this);
-
-        if (!$item.hasClass('disabled')) {
-            navListItems.removeClass('btn-primary').addClass('btn-default');
-            $item.addClass('btn-primary');
-            allWells.hide();
-            $target.show();
-            $target.find('input:eq(0)').focus();
+        var $target = $(e.target);
+    
+        if ($target.parent().hasClass('disabled')) {
+            return false;
         }
     });
 
-
-    allPrevBtn.click(function(){
-        var curStep = $(this).closest(".setup-content"),
-            curStepBtn = curStep.attr("id"),
-            prevStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().prev().children("a");
-
-        prevStepWizard.trigger('click');
-    });
-    
-    allNextBtn.click(function(){
-        var curStep = $(this).closest(".setup-content"),
-            curStepBtn = curStep.attr("id"),
-            nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
-            curInputs = curStep.find("input[type='text'],input[type='url'],input[type='radio']"),
-            isValid = true;
+    $(".next-step").click(function (e) {
         
-        errorClass="alert alert-danger"
+        //added to check validation
+        var activeTab =  $(this).closest(".tab-pane"),
+            curInputs = activeTab.find("input[type='text'],input[type='url'],input[type='radio']"),
+            errorClass="alert alert-danger",
+            isValid = true;
+
+            
         $(".form-group").removeClass(errorClass);
         for(var i=0; i<curInputs.length; i++){
             if (!curInputs[i].validity.valid){
                 isValid = false;
                 $(curInputs[i]).closest(".form-group").addClass(errorClass);
-            } 
+            }
         }
 
-        if (isValid)
-            nextStepWizard.removeAttr('disabled').trigger('click');
-    });
+        if (isValid) {
+            // original
+            var $active = $('.wizard .nav-tabs li.active');
+            $active.next().removeClass('disabled');
+            nextTab($active);
+        }
 
-    $('div.setup-panel div a.btn-primary').trigger('click');
+    });
+    $(".prev-step").click(function (e) {
+
+        var $active = $('.wizard .nav-tabs li.active');
+        prevTab($active);
+
+    });
 });
+
+function nextTab(elem) {
+    $(elem).next().find('a[data-toggle="tab"]').click();
+}
+function prevTab(elem) {
+    $(elem).prev().find('a[data-toggle="tab"]').click();
+}
+
+// End Wizard
+// Begin Custom
+
+$(function () {
+    selectExistingMotherForm();
+    var createNewMotherCB = $('#createNewMotherCB');
+    
+    createNewMotherCB.click(function() {        
+        if(createNewMotherCB.prop('checked') ) {
+            createNewMotherForm();
+        } else {
+            selectExistingMotherForm();
+        }
+
+    });
+     
+    
+    function selectExistingMotherForm() {    
+        // enable select existing mother
+        $('#fieldsetSEM').css('color','#333333').css('background','white');
+        $('#fieldsetSEM legend').css('color','#333333');
+        $('#fieldsetSEM select').attr('disabled',false);
+        
+        //disable create new mother form
+        $('#fieldsetCNM').css('color','gray').css('background','#DCDCDC');
+        $('#fieldsetCNM legend').css('color','gray');
+        $('#fieldsetCNM input').prop('readonly',true);
+        $('#fieldsetCNM select').attr('disabled',true);
+    }
+    
+    function createNewMotherForm(){
+        // disable select existing mother
+        $('#fieldsetSEM').css('color','#gray').css('background','#DCDCDC');
+        $('#fieldsetSEM legend').css('color','gray');
+        $('#fieldsetSEM select').attr('disabled',true);
+        
+        //enable create new mother form
+        $('#fieldsetCNM').css('color','#333333').css('background','white');
+        $('#fieldsetCNM legend').css('color','#333333');
+        $('#fieldsetCNM input').prop('readonly',false);
+        $('#fieldsetCNM select').attr('disabled',false);
+    }
+    
+});
+
+// End Custom
