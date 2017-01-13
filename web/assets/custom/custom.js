b1d44e7944bf77e26b18936f0d5ed04248a71b1a
fafa <fafa@Bouddha>
2017-01-13 12:03:53 +0100

    add mother ++

--- web/assets/custom/custom.js
+++
@@ -1,5 +0,0 @@
- jQuery(document).ready(function(){
-    //$.getScript('assets/custom/js/custom_add_wizard.js');
-    $.getScript('web/assets/custom/js/custom_datatable.js');
-    //$.getScript('assets/custom/js/custom_custom.js');
-});
